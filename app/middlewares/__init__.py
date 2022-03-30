3c13fb28eb9ace777292fe5d5689e0b82d060487
lav <me@lavn.ml>
2022-03-31 00:42:43 +0300

    add auto import for middlewares

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,1 +1,7 @@
-from . import main, throttling
+import os
+from importlib import import_module
+
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == "__init__.py" or module[-3:] != ".py":
+        continue
+    import_module(f".{module[:-3]}", __package__)
