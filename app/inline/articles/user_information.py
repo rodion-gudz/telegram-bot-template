50655cef37e1b1fbb2086c3c8a6275c717d051d5
lav <me@lavn.ml>
2022-03-30 22:33:33 +0300

    inline update

---
+++ app/inline/articles/user_information.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineQueryResultArticle, InputTextMessageContent, User
+
+
+def get_user_information_article(user: User):
+    return InlineQueryResultArticle(
+        id='user_information',
+        title='ℹ️ Информация о пользователе',
+        description=f'Username и ID пользователя {user.full_name}',
+        input_message_content=InputTextMessageContent(
+            message_text=f"<b>🙍‍ Пользователь:</b> @{user.username} \n"
+                         f"<b>📌 ID:</b> <code>{user.id}</code>"
+        )
+    )
---
+++ app/inline/handlers/user_information.py
@@ -0,0 +1,15 @@
+from aiogram.types import InlineQuery
+
+from app import dp
+from app.inline.articles.user_information import get_user_information_article
+
+
+@dp.inline_query()
+async def user_information_query(inline_query: InlineQuery):
+    await inline_query.answer(
+        results=[
+            get_user_information_article(inline_query.from_user)
+        ],
+        cache_time=0,
+        is_personal=True,
+    )
