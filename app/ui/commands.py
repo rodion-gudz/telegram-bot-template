aeb08d2dc60ea8052f8f01c7a401eb13e80d9183
lav <me@lavn.ml>
2022-03-30 22:33:06 +0300

    ui commands setup

---
+++ app/ui/commands.py
@@ -0,0 +1,8 @@
+users_commands = {
+    "start": "Start bot",
+}
+
+owner_commands = {
+    "ping": "Check bot ping",
+    "stats": "Show bot stats",
+}
---
+++ app/ui/setup.py
@@ -0,0 +1,28 @@
+from aiogram import Bot
+from aiogram.types import BotCommandScopeDefault, BotCommand, BotCommandScopeChat
+
+import app
+from app.ui.commands import owner_commands, users_commands
+
+
+async def set_bot_commands(bot: Bot):
+    await bot.set_my_commands(
+        [
+            BotCommand(command=command, description=description)
+            for command, description in owner_commands.items()
+        ],
+        scope=BotCommandScopeChat(chat_id=app.owner_id),
+    )
+
+    await bot.set_my_commands(
+        [
+            BotCommand(command=command, description=description)
+            for command, description in users_commands.items()
+        ],
+        scope=BotCommandScopeDefault(),
+    )
+
+
+async def remove_bot_commands(bot: Bot):
+    await bot.delete_my_commands(scope=BotCommandScopeDefault())
+    await bot.delete_my_commands(scope=BotCommandScopeChat(chat_id=app.owner_id))
