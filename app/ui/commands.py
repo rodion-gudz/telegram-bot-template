b4715b17bb02aa6bea0b575e2c7e222951c50370
lav <me@lavn.ml>
2022-04-18 22:02:31 +0300

    commands update

--- app/ui/commands.py
+++ app/ui/commands.py
@@ -1,3 +1,8 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeChat, BotCommandScopeDefault
+
+import app
+
 users_commands = {
     "support": "Отправить обращение администратору",
     "help": "Показать список команд",
@@ -8,3 +13,26 @@ owner_commands = {
     "ping": "Check bot ping",
     "stats": "Show bot stats",
 }
+
+
+async def set_bot_commands(bot: Bot):
+    await bot.set_my_commands(
+        [
+            BotCommand(command=command, description=description)
+            for command, description in owner_commands.items()
+        ],
+        scope=BotCommandScopeChat(chat_id=app.owner_id),
+    )
+
+    await bot.set_my_commands(
+        [
+            BotCommand(command=command, description=description)
+            for command, description in users_commands.items()
+        ],
+        scope=BotCommandScopeDefault(),
+    )
+
+
+async def remove_bot_commands(bot: Bot):
+    await bot.delete_my_commands(scope=BotCommandScopeDefault())
+    await bot.delete_my_commands(scope=BotCommandScopeChat(chat_id=app.owner_id))
