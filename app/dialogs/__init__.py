2e19e357138054a726f0380f2d6976e3bd192f99
lav <me@lavn.ml>
2022-05-20 14:56:13 +0300

    dialogs package update

--- app/dialogs/__init__.py
+++ app/dialogs/__init__.py
@@ -1,1 +1,10 @@
-from .ui import *
+import os
+from importlib import import_module
+
+from app import registry
+
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == "__init__.py" or module[-3:] != ".py":
+        continue
+    registry.register(getattr(import_module(
+        f".{module[:-3]}", __package__), "ui"))
