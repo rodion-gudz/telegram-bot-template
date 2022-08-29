ca74780c1c8c8c03dc2469315b8023ac09fd1680
lav <me@lavn.ml>
2022-08-30 01:07:13 +0300

    remove `bot` argument from main middleware

--- app/middlewares/main.py
+++ app/middlewares/main.py
@@ -3,13 +3,12 @@ from typing import Any, Awaitable, Callable, Dict
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
-from app import bot, client, config, dp
+from app import client, config, dp
 
 
 class MainMiddleware(BaseMiddleware):
     def __init__(self) -> None:
         self.config = config
-        self.bot = bot
         self.client = client
 
     async def __call__(
@@ -18,7 +17,6 @@ class MainMiddleware(BaseMiddleware):
         event: Message,
         data: Dict[str, Any],
     ) -> Any:
-        data["bot"] = self.bot
         data["client"] = self.client
         await handler(event, data)
 
