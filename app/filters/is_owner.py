13c0f5cc0316236f31e1be6f41c44749e2f7f5d8
lav <me@lavn.ml>
2022-10-10 14:07:19 +0300

    update `aiogram` and `aiogram-dialog`

--- app/filters/is_owner.py
+++ app/filters/is_owner.py
@@ -1,11 +1,13 @@
 from aiogram import types
-from aiogram.filters import BaseFilter
+from aiogram.filters import Filter
 
 from app.config import Config
 
 
-class IsOwner(BaseFilter):
-    is_owner: bool
+class IsOwner(Filter):
+
+    def __init__(self, is_owner: bool) -> None:
+        self.is_owner = is_owner
 
     async def __call__(self, message: types.Message, config: Config) -> bool:
         return self.is_owner is (message.from_user.id == config.settings.owner_id)
